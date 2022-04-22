6c967e279bc075d31e2d1876f10d39c84ef773d3
Artik Zenevich <azenevich91@gmail.com>
2022-04-22 15:26:43 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="flex justify-center items-center h-screen">
+		<h1 class="text-3xl font-bold underline">Hello {name}!</h1>
+	</div>
+</main>
+
+<style>
+
+</style>
